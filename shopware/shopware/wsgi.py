ca3891a96d33a7bb3169bf78bb07ede421feda19
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-12-14 03:29:38 +0100

    changed gunicorn version

--- shopware/shopware/wsgi.py
+++ shopware/shopware/wsgi.py
@@ -1,15 +1,10 @@
-"""
-WSGI config for shopware project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+import sys
+
+print(sys.path)
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopware.settings')
 
